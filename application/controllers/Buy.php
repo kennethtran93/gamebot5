e1ae3d74e4fd0543cb1cbbfbb2112d2fd5ac1674
josephwithay <yoseph_jo@hotmail.com>
2016-04-12 18:20:46 -0700

    Buy function completed Signed-off-by:josephwithay <yoseph_jo@hotmail.com>

--- application/controllers/Buy.php
+++ application/controllers/Buy.php
@@ -6,14 +6,28 @@ class Buy extends Application
 	 * Contructor
 	 */
 
-	function __contruc()
+	function __construct()
 	{
 		parent::__construct();
 	}
-	
+
 	function index()
 	{
-		
+		$this->purchaseCard();
+	}
+
+	private function purchaseCard()
+	{
+
+		$team = "A05"; // team name
+		$token = "6d5d68ace9f79b2eb469f486b651a40d"; // token, team must have been registered
+		$player = $this->session->userdata('username'); // current playername stored in session data
+		$success = $this->players->buy($team, $token, $player);
+
+		if ($success)
+		{
+			echo 'The card pack is purchased.';
+		}
 	}
 
 }
--- application/models/Players.php
+++ application/models/Players.php
@@ -6,12 +6,37 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Data access wrapper for "Players" table.
  *
  */
-class Players extends MY_Model {
+class Players extends MY_Model
+{
 
 	// constructor
 	function __construct()
 	{
 		parent::__construct('players', 'Player');
+	}
+
+	function buy($team, $token, $player)
+	{
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "http://botcards.jlparry.com/buy");
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_POST, true);
+		$result = curl_exec($ch);
+		curl_close($ch);
+		$xml = simplexml_load_string($result);
+
+		foreach ($xml->certificate as $certificate)
+		{
+			$timestamp = date(DATE_ATOM, (int) $certificate->datetime);
+			$data = array(
+				'token' => (string) $certificate->token,
+				'piece' => (string) $certificate->piece,
+				'broker' => (string) $certificate->broker,
+				'player' => (string) $certificate->player,
+				'datetime' => $timestamp
+			);
+			$this->db->insert('collections', $data); // insert into database
+		}
 	}
 
 }
