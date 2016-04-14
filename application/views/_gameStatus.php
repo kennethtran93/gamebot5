a4f24f840456c9e375bb65f6e6e1be81deeeb5c1
Kenneth Tran <kennethtran93@gmail.com>
2016-04-14 11:27:13 -0700

    Added Admin Promote/Demote to Player management.  Optimized back-end code of Admin Controller.  Status messages after actions of player management fixed. Signed-off-by:Kenneth Tran <kennethtran93@gmail.com>

--- application/views/_gameStatus.php
+++ application/views/_gameStatus.php
@@ -8,6 +8,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div id="game_status">
 	<p>
 		<b>Game Server Status:</b><br />
-		Round: <b>{round}</b> | State: <b>{state} ({desc})</b> | Countdown:  <b>{countdown} second{s}</b> until state is {upcoming} (at {alarm}) | Current Game/Server Time:  <b>{now}</b>
+		Round: <b>{round}</b> | State: <b>{state} ({current})</b> | Countdown:  <b>{countdown} second{s}</b> until state is {upcoming} (at {alarm}) | Current Game/Server Time:  <b>{now}</b>
 	</p>
 </div>
